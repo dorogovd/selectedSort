b4300c79fe8285e4e6db23a745a8aef46a23aea3
Dima <dorogovd@yandex.ru>
2022-12-07 01:02:17 +0300

    readLine() Loop
    For loop added

--- a2Test/main.swift
+++ a2Test/main.swift
@@ -12,7 +12,7 @@ func selectionSort(_ array: [Int]) {
     var internalArray = array
     print("\nSELECTION SORT - DOOSHNILA EDITION\n \ndefault array = \(internalArray)\n")
     
-    var indexes = Indexes(
+    let indexes = Indexes(
         smallestIndex: internalArray.startIndex,
         comparedIndex: internalArray.startIndex + 1
     )
@@ -91,7 +91,7 @@ func selectionSort(_ array: [Int]) {
 
 func sortLogicAndPrintResult(numberOfIteration: Double, smallestIndex: Int, comparedIndex: Int, internalArray: [Int]) -> Indexes {
 
-    var result = internalArray[comparedIndex] < internalArray[smallestIndex]
+    let result = internalArray[comparedIndex] < internalArray[smallestIndex]
     var tempSmallestIndex = smallestIndex
     var tempComparedIndex = comparedIndex
 
@@ -111,39 +111,22 @@ func sortLogicAndPrintResult(numberOfIteration: Double, smallestIndex: Int, comp
     return Indexes(smallestIndex: tempSmallestIndex, comparedIndex: tempComparedIndex)
 }
 
-//var array = [3,1,88,4,0]
-//var array = [1,3,6,2,10]
-//var array = [5, 3, 6, 2, 10]
-//var array = [1, 2, 5, 0, 9]
-var array = [10, 9, 2, 0, 6]
-selectionSort(array)
+var array = Array<Int>()
+print("Create an array of 5 numbers.")
+for _ in 0...4 {
+    print("\nEnter a number:")
+    let item = readLine()
+    array.append(Int(item!)!)
+    guard Int(item!) != nil else {
+        fatalError("It is not a number")
+    }
+}
+    selectionSort(array)
+    
+    //var array = [3,1,88,4,0]
+    //var array = [1,3,6,2,10]
+    //var array = [5, 3, 6, 2, 10]
+    //var array = [1, 2, 5, 0, 9]
+    //var array = [10, 9, 2, 0, 6]
 
-//var array = [Int(item1), Int(item2), Int(item3), Int(item4), Int(item5)]
-
-
-
-print("Enter your first number in array")
-let item1 = readLine()
-guard let item1 = Int(item1!) else {
-    fatalError("It is not a number")
-}
-print("Enter your second number in array")
-let item2 = readLine()
-guard let item2 = Int(item2!) else {
-    fatalError("It is not a number")
-}
-print("Enter your third number in array")
-let item3 = readLine()
-guard let item3 = Int(item3!) else {
-    fatalError("It is not a number")
-}
-print("Enter your fourth number in array")
-let item4 = readLine()
-guard let item4 = Int(item4!) else {
-    fatalError("It is not a number")
-}
-print("Enter your fifth number in array")
-let item5 = readLine()
-guard let item5 = Int(item5!) else {
-    fatalError("It is not a number")
-}
+// var array = [Int(item1), Int(item2), Int(item3), Int(item4), Int(item5)]
