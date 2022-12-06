951dabae9b4326820ccecd62685198a33665ca50
Dima <dorogovd@yandex.ru>
2022-12-06 22:07:49 +0300

    working code w/ indexes class
    - fixed idx constants
    - updated print spaces
    - updated name of sort method

--- a2Test/main.swift
+++ a2Test/main.swift
@@ -10,105 +10,86 @@ import Foundation
 func selectionSort(_ array: [Int]) {
     
     var internalArray = array
-    print("""
-
-SELECTION SORT - DOOSHNILA EDITION
-
-default array = \(internalArray)
-
-""")
+    print("\nSELECTION SORT - DOOSHNILA EDITION\n \ndefault array = \(internalArray)\n")
     
     var indexes = Indexes(
         smallestIndex: internalArray.startIndex,
         comparedIndex: internalArray.startIndex + 1
     )
     
-    let idx0 = printResult(numberOfIteration: 1, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
+    let idx0 = sortLogicAndPrintResult(numberOfIteration: 1, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
     
     if  idx0.smallestIndex == idx0.comparedIndex {
         idx0.comparedIndex = 2
     }
     
-//    let idx1 = printResult(numberOfIteration: 1.1, smallestIndex: idx0, comparedIndex: &comparedIndex, internalArray: internalArray)
-//    if  idx1 == comparedIndex {
-//        comparedIndex = 3
-//    }
-//
-//    let idx2 = printResult(numberOfIteration: 1.2, smallestIndex: idx1, comparedIndex: &comparedIndex, internalArray: internalArray)
-//    if  idx2 == comparedIndex {
-//        comparedIndex = 4
-//    }
-//
-//    let idx3 = printResult(numberOfIteration: 1.3, smallestIndex: idx2, comparedIndex: &comparedIndex, internalArray: internalArray)
-//
-//    internalArray.insert(internalArray[idx3], at: 0)
-//    internalArray.remove(at: idx3 + 1)
-//
-//    print("""
-//            1st cycle result - \(internalArray)
-//
-//            """)
-//
-//    smallestIndex = internalArray.startIndex + 1
-//    comparedIndex = 2
-//
-//    let idx4 = printResult(numberOfIteration: 2, smallestIndex: smallestIndex, comparedIndex: &comparedIndex, internalArray: internalArray)
-//    if  idx4 == comparedIndex {
-//        comparedIndex = 3
-//    }
-//
-//    let idx5 = printResult(numberOfIteration: 2.1, smallestIndex: idx4, comparedIndex: &comparedIndex, internalArray: internalArray)
-//    if  idx5 == comparedIndex {
-//        comparedIndex = 4
-//    }
-//
-//    let idx6 = printResult(numberOfIteration: 2.2, smallestIndex: idx5, comparedIndex: &comparedIndex, internalArray: internalArray)
-//
-//    internalArray.insert(internalArray[idx6], at: 1)
-//    internalArray.remove(at: idx6 + 1)
-//
-//    print("""
-//            2nd cycle result - \(internalArray)
-//
-//            """)
-//
-//    smallestIndex = internalArray.startIndex + 2
-//    comparedIndex = 3
-//
-//    let idx7 = printResult(numberOfIteration: 3, smallestIndex: smallestIndex, comparedIndex: &comparedIndex, internalArray: internalArray)
-//    if  idx7 == comparedIndex {
-//        comparedIndex = 4
-//    }
-//
-//    let idx8 = printResult(numberOfIteration: 3.1, smallestIndex: idx7, comparedIndex: &comparedIndex, internalArray: internalArray)
+    let idx1 = sortLogicAndPrintResult(numberOfIteration: 1.1, smallestIndex: idx0.smallestIndex, comparedIndex: idx0.comparedIndex, internalArray: internalArray)
     
-//    internalArray.insert(internalArray[idx8], at: 2)
-//    internalArray.remove(at: idx8 + 1)
+    if  idx1.smallestIndex == idx1.comparedIndex {
+        idx1.comparedIndex = 3
+    }
+
+    let idx2 = sortLogicAndPrintResult(numberOfIteration: 1.2, smallestIndex: idx1.smallestIndex, comparedIndex: idx1.comparedIndex, internalArray: internalArray)
+    if  idx2.smallestIndex == idx2.comparedIndex {
+        idx2.comparedIndex = 4
+    }
+
+    let idx3 = sortLogicAndPrintResult(numberOfIteration: 1.3, smallestIndex: idx2.smallestIndex, comparedIndex: idx2.comparedIndex, internalArray: internalArray)
+
+    internalArray.insert(internalArray[idx3.smallestIndex], at: 0)
+    internalArray.remove(at: idx3.smallestIndex + 1)
+
+    print("1st cycle result - \(internalArray)\n")
+
+    indexes.smallestIndex = internalArray.startIndex + 1
+    indexes.comparedIndex = 2
+
+    let idx4 = sortLogicAndPrintResult(numberOfIteration: 2, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
+    if  idx4.smallestIndex == idx4.comparedIndex {
+        idx4.comparedIndex = 3
+    }
+
+    let idx5 = sortLogicAndPrintResult(numberOfIteration: 2.1, smallestIndex: idx4.smallestIndex, comparedIndex: idx4.comparedIndex, internalArray: internalArray)
     
+    if  idx5.smallestIndex == idx5.comparedIndex {
+        idx5.comparedIndex = 4
+    }
+
+    let idx6 = sortLogicAndPrintResult(numberOfIteration: 2.2, smallestIndex: idx5.smallestIndex, comparedIndex: idx5.comparedIndex, internalArray: internalArray)
+
+    internalArray.insert(internalArray[idx6.smallestIndex], at: 1)
+    internalArray.remove(at: idx6.smallestIndex + 1)
+
+    print("2nd cycle result - \(internalArray)\n")
+
+    indexes.smallestIndex = internalArray.startIndex + 2
+    indexes.comparedIndex = 3
+
+    let idx7 = sortLogicAndPrintResult(numberOfIteration: 3, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
     
-    print("""
-            3rd cycle result - \(internalArray)
-            
-            """)
+    if  idx7.smallestIndex == idx7.comparedIndex {
+        idx7.comparedIndex = 4
+    }
+
+    let idx8 = sortLogicAndPrintResult(numberOfIteration: 3.1, smallestIndex: idx7.smallestIndex, comparedIndex: idx7.comparedIndex, internalArray: internalArray)
     
-//    smallestIndex = internalArray.startIndex + 3
-//    comparedIndex = 4
+    internalArray.insert(internalArray[idx8.smallestIndex], at: 2)
+    internalArray.remove(at: idx8.smallestIndex + 1)
     
-//    let idx9 = printResult(numberOfIteration: 4.0, smallestIndex: smallestIndex, comparedIndex: &comparedIndex, internalArray: internalArray)
-//
-//    internalArray.insert(internalArray[idx9], at: 3)
-//    internalArray.remove(at: idx9 + 1)
+    print("3rd cycle result - \(internalArray)\n")
     
+    indexes.smallestIndex = internalArray.startIndex + 3
+    indexes.comparedIndex = 4
     
-    print("""
-            FINAL RESULT - \(internalArray)
-            
-            ARRAY IS SORTED
-            
-            """)
+    let idx9 = sortLogicAndPrintResult(numberOfIteration: 4.0, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
+
+    internalArray.insert(internalArray[idx9.smallestIndex], at: 3)
+    internalArray.remove(at: idx9.smallestIndex + 1)
+    
+    print("FINAL RESULT - \(internalArray)\n \nARRAY IS SORTED\n")
 }
 
-func printResult(numberOfIteration: Double, smallestIndex: Int, comparedIndex: Int, internalArray: [Int]) -> Indexes {
+func sortLogicAndPrintResult(numberOfIteration: Double, smallestIndex: Int, comparedIndex: Int, internalArray: [Int]) -> Indexes {
 
     var result = internalArray[comparedIndex] < internalArray[smallestIndex]
     var tempSmallestIndex = smallestIndex
@@ -130,11 +111,11 @@ func printResult(numberOfIteration: Double, smallestIndex: Int, comparedIndex: I
     return Indexes(smallestIndex: tempSmallestIndex, comparedIndex: tempComparedIndex)
 }
 
-var array = [3,1,88,4,0]
+//var array = [3,1,88,4,0]
 //var array = [1,3,6,2,10]
 //var array = [5, 3, 6, 2, 10]
 //var array = [1, 2, 5, 0, 9]
-//var array = [10, 9, 2, 0, 6]
+var array = [10, 9, 2, 0, 6]
 selectionSort(array)
 
 //var array = [Int(item1), Int(item2), Int(item3), Int(item4), Int(item5)]
