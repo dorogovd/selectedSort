88450a04ec332c3906c8126f597faae088e4cfd6
Dima <dorogovd@yandex.ru>
2022-12-10 21:48:05 +0300

    Pull request tasks completed
    - func sortLogicAndPrintResult name changed to defineIndexes
    - func defineIndexes now accept only one argument of Index type
    - conditions "if" moved from selectionSort to defineIndexes
    - readLine loop is placed in createArray method

--- a2Test/main.swift
+++ a2Test/main.swift
@@ -17,24 +17,13 @@ func selectionSort(_ array: [Int]) {
         comparedIndex: internalArray.startIndex + 1
     )
     
-    let idx0 = sortLogicAndPrintResult(numberOfIteration: 1, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
+    let idx0 = defineIndexes(numberOfIteration: 1, indexes: indexes, ifIndexesIsEqualValue: 2, internalArray: internalArray)
     
-    if  idx0.smallestIndex == idx0.comparedIndex {
-        idx0.comparedIndex = 2
-    }
-    
-    let idx1 = sortLogicAndPrintResult(numberOfIteration: 1.1, smallestIndex: idx0.smallestIndex, comparedIndex: idx0.comparedIndex, internalArray: internalArray)
-    
-    if  idx1.smallestIndex == idx1.comparedIndex {
-        idx1.comparedIndex = 3
-    }
+    let idx1 = defineIndexes(numberOfIteration: 1.1, indexes: idx0, ifIndexesIsEqualValue: 3, internalArray: internalArray)
 
-    let idx2 = sortLogicAndPrintResult(numberOfIteration: 1.2, smallestIndex: idx1.smallestIndex, comparedIndex: idx1.comparedIndex, internalArray: internalArray)
-    if  idx2.smallestIndex == idx2.comparedIndex {
-        idx2.comparedIndex = 4
-    }
+    let idx2 = defineIndexes(numberOfIteration: 1.2, indexes: idx1, ifIndexesIsEqualValue: 4, internalArray: internalArray)
 
-    let idx3 = sortLogicAndPrintResult(numberOfIteration: 1.3, smallestIndex: idx2.smallestIndex, comparedIndex: idx2.comparedIndex, internalArray: internalArray)
+    let idx3 = defineIndexes(numberOfIteration: 1.3, indexes: idx2, ifIndexesIsEqualValue: 2, internalArray: internalArray)
 
     internalArray.insert(internalArray[idx3.smallestIndex], at: 0)
     internalArray.remove(at: idx3.smallestIndex + 1)
@@ -44,18 +33,11 @@ func selectionSort(_ array: [Int]) {
     indexes.smallestIndex = internalArray.startIndex + 1
     indexes.comparedIndex = 2
 
-    let idx4 = sortLogicAndPrintResult(numberOfIteration: 2, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
-    if  idx4.smallestIndex == idx4.comparedIndex {
-        idx4.comparedIndex = 3
-    }
+    let idx4 = defineIndexes(numberOfIteration: 2, indexes: indexes, ifIndexesIsEqualValue: 3, internalArray: internalArray)
 
-    let idx5 = sortLogicAndPrintResult(numberOfIteration: 2.1, smallestIndex: idx4.smallestIndex, comparedIndex: idx4.comparedIndex, internalArray: internalArray)
-    
-    if  idx5.smallestIndex == idx5.comparedIndex {
-        idx5.comparedIndex = 4
-    }
+    let idx5 = defineIndexes(numberOfIteration: 2.1, indexes: idx4, ifIndexesIsEqualValue: 4, internalArray: internalArray)
 
-    let idx6 = sortLogicAndPrintResult(numberOfIteration: 2.2, smallestIndex: idx5.smallestIndex, comparedIndex: idx5.comparedIndex, internalArray: internalArray)
+    let idx6 = defineIndexes(numberOfIteration: 2.2, indexes: idx5, ifIndexesIsEqualValue: 3, internalArray: internalArray)
 
     internalArray.insert(internalArray[idx6.smallestIndex], at: 1)
     internalArray.remove(at: idx6.smallestIndex + 1)
@@ -65,13 +47,9 @@ func selectionSort(_ array: [Int]) {
     indexes.smallestIndex = internalArray.startIndex + 2
     indexes.comparedIndex = 3
 
-    let idx7 = sortLogicAndPrintResult(numberOfIteration: 3, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
-    
-    if  idx7.smallestIndex == idx7.comparedIndex {
-        idx7.comparedIndex = 4
-    }
+    let idx7 = defineIndexes(numberOfIteration: 3, indexes: indexes, ifIndexesIsEqualValue: 4, internalArray: internalArray)
 
-    let idx8 = sortLogicAndPrintResult(numberOfIteration: 3.1, smallestIndex: idx7.smallestIndex, comparedIndex: idx7.comparedIndex, internalArray: internalArray)
+    let idx8 = defineIndexes(numberOfIteration: 3.1, indexes: idx7, ifIndexesIsEqualValue: 4, internalArray: internalArray)
     
     internalArray.insert(internalArray[idx8.smallestIndex], at: 2)
     internalArray.remove(at: idx8.smallestIndex + 1)
@@ -81,7 +59,7 @@ func selectionSort(_ array: [Int]) {
     indexes.smallestIndex = internalArray.startIndex + 3
     indexes.comparedIndex = 4
     
-    let idx9 = sortLogicAndPrintResult(numberOfIteration: 4.0, smallestIndex: indexes.smallestIndex, comparedIndex: indexes.comparedIndex, internalArray: internalArray)
+    let idx9 = defineIndexes(numberOfIteration: 4.0, indexes: indexes, ifIndexesIsEqualValue: 0, internalArray: internalArray)
 
     internalArray.insert(internalArray[idx9.smallestIndex], at: 3)
     internalArray.remove(at: idx9.smallestIndex + 1)
@@ -89,20 +67,18 @@ func selectionSort(_ array: [Int]) {
     print("FINAL RESULT - \(internalArray)\n \nARRAY IS SORTED\n")
 }
 
-func sortLogicAndPrintResult(numberOfIteration: Double, smallestIndex: Int, comparedIndex: Int, internalArray: [Int]) -> Indexes {
+func defineIndexes(numberOfIteration: Double, indexes: Indexes, ifIndexesIsEqualValue: Int, internalArray: [Int]) -> Indexes {
 
-    let result = internalArray[comparedIndex] < internalArray[smallestIndex]
-    var tempSmallestIndex = smallestIndex
-    var tempComparedIndex = comparedIndex
+    let result = internalArray[indexes.comparedIndex] < internalArray[indexes.smallestIndex]
+    var tempSmallestIndex = indexes.smallestIndex
+    var tempComparedIndex = indexes.comparedIndex
+    
 
-    print("""
-    ITERATION \(numberOfIteration) :
-    compared numbers: \(internalArray[comparedIndex]) < \(internalArray[tempSmallestIndex]) -> \(result)
-
-    """)
+    print("ITERATION \(numberOfIteration): compared numbers: \(internalArray[indexes.comparedIndex]) < \(internalArray[tempSmallestIndex]) -> \(result)\n")
     if result == true {
-        tempSmallestIndex = comparedIndex
-    } else if comparedIndex == 4 && result == false {
+        tempSmallestIndex = indexes.comparedIndex
+        tempComparedIndex = ifIndexesIsEqualValue
+    } else if indexes.comparedIndex == 4 && result == false {
         tempComparedIndex = tempSmallestIndex + 1
     } else {
         tempComparedIndex += 1
@@ -111,22 +87,25 @@ func sortLogicAndPrintResult(numberOfIteration: Double, smallestIndex: Int, comp
     return Indexes(smallestIndex: tempSmallestIndex, comparedIndex: tempComparedIndex)
 }
 
-var array = Array<Int>()
-print("Create an array of 5 numbers.")
-for _ in 0...4 {
-    print("\nEnter a number:")
-    let item = readLine()
-    array.append(Int(item!)!)
-    guard Int(item!) != nil else {
+func createArray(_: Int) -> [Int] {
+    var array = Array<Int>()
+    for _ in 0...4 {
+        print("\nEnter a number:")
+        let item = readLine()
+        array.append(Int(item!)!)
+        guard Int(item!) != nil else {
         fatalError("It is not a number")
+        }
     }
-}
-    selectionSort(array)
     
-    //var array = [3,1,88,4,0]
-    //var array = [1,3,6,2,10]
-    //var array = [5, 3, 6, 2, 10]
-    //var array = [1, 2, 5, 0, 9]
-    //var array = [10, 9, 2, 0, 6]
+    return array
+}
 
-// var array = [Int(item1), Int(item2), Int(item3), Int(item4), Int(item5)]
+selectionSort(createArray(5))
+    
+//array examples:
+//var array = [3,1,88,4,0]
+//var array = [1,3,6,2,10]
+//var array = [5, 3, 6, 2, 10]
+//var array = [1, 2, 5, 0, 9]
+//var array = [10, 9, 2, 0, 6]
